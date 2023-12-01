7fa527362290d4c8104663f8b0702ad19c6fd144
Mane <mane.ghudratyan@gmail.com>
2023-12-01 17:41:04 +0400

    Write simple example with callback function

---
+++ Homework2/useCallback.dart
@@ -0,0 +1,12 @@
+void printMessage(String Function() callback){
+  String result = callback();
+  print("Result from callback: $result");
+}
+
+String myFunc(){
+  return "Hello World!";
+}
+
+void main(){
+  printMessage(() => myFunc());
+}
